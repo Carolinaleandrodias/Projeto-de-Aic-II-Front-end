b3ea3231fdc5cb00356b0116302db898e4189397
Arthur Castro <96489620+ArthurALDC@users.noreply.github.com>
2024-11-04 22:10:46 -0300

    Delete AICIISCRUD/app directory

--- AICIISCRUD/app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'minha_chave_secreta'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'  # Exemplo de URI para SQLite
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
- 
--- AICIISCRUD/app/database.py
+++
@@ -1,3 +0,0 @@
-# app/database.py
-from flask_sqlalchemy import SQLAlchemy
-sql_db = SQLAlchemy()
